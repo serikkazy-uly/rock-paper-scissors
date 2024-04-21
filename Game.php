bb6200f888ffde5a95a87e640c3821426210ca64
serikkazy-uly <damirkap89@gmail.com>
2024-04-21 19:12:21 +0500

    feat: add userMove step

--- Game.php
+++ Game.php
@@ -24,6 +24,15 @@ class Game
 
     public function userMove()
     {
+        do {
+            echo "Enter your move: ";
+            $input = readline();
+           
+    
+            $moveIndex = intval($input) - 1;
+        } while (!is_numeric($input) || $moveIndex < 0 || $moveIndex >= count($this->moves));
+    
+        return $this->moves[$moveIndex];
     }
 
     public function getResult($userMove, $compMove)
